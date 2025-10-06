3f79d07d4290e0abdc2608479e97ff279c4f89ce
Pedro Baraldi Guerreiro <pedrobaraldi2210@gmail.com>
2025-10-06 16:05:24 -0300

    Implementation of adding products do the card and improvements

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -1,46 +1,46 @@
 describe('Login', () => {
     it('Realizar login com sucesso', () => {
         // Arrange
-        cy.visit('https://www.saucedemo.com/v1/');
+        cy.visit('https://www.saucedemo.com');
 
         // Act
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').type('secret_sauce');
-        cy.contains('LOGIN').click();
+        cy.contains('Login').click();
 
 
         // Assert
-        cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html');
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
 
     })
 
     it('Realizar login com senha incorreta', () => {
         // Arrange
-        cy.visit('https://www.saucedemo.com/v1/');
+        cy.visit('https://www.saucedemo.com');
         
         // Act
         cy.get('[data-test="username"]').type('USUARIO INVALIDO');
         cy.get('[data-test="password"]').type('SENHA INVALIDA');
-        cy.contains('LOGIN').click();
+        cy.contains('Login').click();
 
         // Assert
-       cy.get('[data-test="error"]').should('contain.text','Username and password do not match any user in this service')
-       cy.url().should('eq', 'https://www.saucedemo.com/v1/');
+       cy.get('[data-test="error"]').should('contain.text','Epic sadface: Username and password do not match any user in this service')
+       cy.url().should('eq', 'https://www.saucedemo.com/');
     })
 
 })
 
 describe('Incluindo produtos no carrinho', () => {
-    it('Adicionar um produto ao carrinho', () => {
+    it('Adicionar qualquer produto ao carrinho', () => {
     // Arrange
-    cy.visit('https://www.saucedemo.com/v1/');
+    cy.visit('https://www.saucedemo.com');
 
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
-    cy.contains('LOGIN').click();
+    cy.contains('Login').click();
 
     // Act
-    cy.contains('button', 'ADD TO CART').click();
+    cy.contains('button', 'Add to cart').click();
 
     // Assert
     cy.get('.shopping_cart_badge').should('have.text', '1');
@@ -49,19 +49,42 @@ describe('Incluindo produtos no carrinho', () => {
 
     it('Removendo um produto do carrinho', () => {
     // Arrange
-    cy.visit('https://www.saucedemo.com/v1/');
+    cy.visit('https://www.saucedemo.com');
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
-    cy.contains('LOGIN').click();
-    cy.contains('button', 'ADD TO CART').click();
+    cy.contains('Login').click();
+    cy.contains('button', 'Add to cart').click();
     
 
     // Act
-    cy.contains('button', 'REMOVE').click();
+    cy.contains('button', 'Remove').click();
 
     // Assert
     cy.get('.shopping_cart_badge').should('not.exist');
-
-
     })
+
+    it('Adicionar mochila e camiseta ao carrinho', () => {
+    // Arrange
+    cy.visit('https://www.saucedemo.com');
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.contains('Login').click();
+
+    // Act
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    cy.get('.shopping_cart_badge').click();
+
+    // Assert
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.url().should('eq', 'https://www.saucedemo.com/cart.html');
+    cy.get('.cart_item').should('have.length', 2);
+    cy.get('.shopping_cart_badge')
+  .invoke('text')
+  .then((t) => {
+    const n = Number(t.trim());
+    expect(n).to.equal(2);
+  });
+
+})
 })
